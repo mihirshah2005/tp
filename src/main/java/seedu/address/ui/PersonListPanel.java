1d3841a3a327e57b7d47eb9bd092e50f4a6363bb
mihirshah2005 <e1427144@u.nus.edu>
2025-10-20 21:15:31 +0800

    Update GUI to create splitview

--- src/main/java/seedu/address/ui/PersonListPanel.java
+++ src/main/java/seedu/address/ui/PersonListPanel.java
@@ -3,47 +3,69 @@ package seedu.address.ui;
 import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.Region;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.model.person.Person;
+import seedu.address.model.person.Student;
+import seedu.address.model.person.Volunteer;
 
 /**
- * Panel containing the list of persons.
+ * Panel showing two side-by-side lists: Students (left) and Volunteers (right).
  */
 public class PersonListPanel extends UiPart<Region> {
     private static final String FXML = "PersonListPanel.fxml";
     private final Logger logger = LogsCenter.getLogger(PersonListPanel.class);
 
-    @FXML
-    private ListView<Person> personListView;
+    @FXML private ListView<Person> studentListView;
+    @FXML private ListView<Person> volunteerListView;
 
-    /**
-     * Creates a {@code PersonListPanel} with the given {@code ObservableList}.
-     */
-    public PersonListPanel(ObservableList<Person> personList) {
+    private final ObservableList<Person> masterList;
+
+    public PersonListPanel(ObservableList<Person> masterList) {
         super(FXML);
-        personListView.setItems(personList);
-        personListView.setCellFactory(listView -> new PersonListViewCell());
+        this.masterList = masterList;
+
+        FilteredList<Person> students = new FilteredList<>(masterList, p -> p instanceof Student);
+        FilteredList<Person> volunteers = new FilteredList<>(masterList, p -> p instanceof Volunteer);
+
+        studentListView.setItems(students);
+        volunteerListView.setItems(volunteers);
+
+        studentListView.setCellFactory(lv -> new PersonListViewCell(this.masterList));
+        volunteerListView.setCellFactory(lv -> new PersonListViewCell(this.masterList));
+
+        // when the master list order/contents change, refresh both lists so numbers will update
+        this.masterList.addListener((javafx.collections.ListChangeListener<Person>) c -> {
+            studentListView.refresh();
+            volunteerListView.refresh();
+        });
     }
 
     /**
-     * Custom {@code ListCell} that displays the graphics of a {@code Person} using a {@code PersonCard}.
+     * Custom cell that shows a Person via PersonCard.
      */
-    class PersonListViewCell extends ListCell<Person> {
+    static class PersonListViewCell extends ListCell<Person> {
+        private final ObservableList<Person> masterList;
+
+        PersonListViewCell(ObservableList<Person> masterList) {
+            this.masterList = masterList;
+        }
+
         @Override
         protected void updateItem(Person person, boolean empty) {
             super.updateItem(person, empty);
-
             if (empty || person == null) {
                 setGraphic(null);
                 setText(null);
-            } else {
-                setGraphic(new PersonCard(person, getIndex() + 1).getRoot());
+                return;
             }
+            // indexes here are based on the master list so that I don't break the pair function
+            int globalIndex = masterList.indexOf(person) + 1;
+            setGraphic(new PersonCard(person, globalIndex).getRoot());
         }
     }
-
 }
