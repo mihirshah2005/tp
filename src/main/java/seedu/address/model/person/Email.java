0cfb00313545df169a6aeb47257f2c0e7097aca3
recursive-rc <178076079+recursive-rc@users.noreply.github.com>
2025-11-03 23:20:38 +0800

    Fix incorrect information in message on email constraints

--- src/main/java/seedu/address/model/person/Email.java
+++ src/main/java/seedu/address/model/person/Email.java
@@ -12,15 +12,23 @@ public class Email {
     private static final String SPECIAL_CHARACTERS = "+_.-";
     public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@domain "
             + "and adhere to the following constraints:\n"
-            + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
-            + "the parentheses, (" + SPECIAL_CHARACTERS + "). The local-part may not start or end with any special "
-            + "characters.\n"
+            + "1. The local-part should:\n"
+            + "    - start with a letter of the alphabet or a numerical digit\n"
+            + "    - end with a letter of the alphabet or a numerical digit\n"
+            + "    - not contain anything other than the letter of the alphabet, a numerical digit, "
+            + "and these special characters, excluding the parentheses, (" + SPECIAL_CHARACTERS + ")\n"
+            + "    - not have any two special characters adjacent to each other\n"
             + "2. This is followed by a '@' and then a domain name. The domain name is made up of domain labels "
             + "separated by periods.\n"
             + "The domain name must:\n"
             + "    - end with a domain label at least 2 characters long\n"
-            + "    - have each domain label start and end with alphanumeric characters\n"
-            + "    - have each domain label consist of alphanumeric characters, separated only by hyphens, if any.";
+            + "Also, each domain label in the domain name must:\n"
+            + "    - start a letter of the alphabet or a numerical digit\n"
+            + "    - end a letter of the alphabet or a numerical digit\n"
+            + "    - not contain anything other than the letter of the alphabet, a numerical digit, or hyphens\n"
+            + "    - not have any two hyphens adjacent to each other.\n"
+            + "Note: The word \"alphabet\" here refers to the 26 letters of the English alphabet. "
+            + "For this app, other letters, such as é and æ, cannot be used in an email address.";
     // alphanumeric and special characters
     private static final String ALPHANUMERIC_NO_UNDERSCORE = "[^\\W_]+"; // alphanumeric characters except underscore
     private static final String LOCAL_PART_REGEX = "^" + ALPHANUMERIC_NO_UNDERSCORE + "([" + SPECIAL_CHARACTERS + "]"
